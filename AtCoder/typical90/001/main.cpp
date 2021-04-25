6047076a3b4ac81f6b2658aa886d296600108493
aoken7 <aokensabu1@gmail.com>
2021-04-25 14:51:37 +0900

    update

--- AtCoder/typical90/001/main.cpp
+++ AtCoder/typical90/001/main.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define all(a) a.begin(), a.end()
-#define put(i) cout<<i<<endl
-#define rep(i,s,n) for(long long i=s;i<(long long)(n);i++)
+#define put(i) cout << i << endl
+#define rep(i, s, n) for (long long i = s; i < (long long)(n); i++)
 using namespace std;
 using ll = long long;
 
@@ -11,20 +11,59 @@ void putl(T t) {
     cout << endl;
 }
 
-int main(){
-    ll n,l; cin >> n >> l;
-    ll k; cin >> k;
-    vector<ll> a(n);
-    rep(i,0,n) cin >> a[i];
+ll n, l, k;
+vector<ll> a, b;
 
-    vector<pair<ll,ll>> b(n-1,{0,0});
-    rep(i,0,n-1){
-        b[i] = {a[i+1] - a[i], i};
+// 1ブロックをx以上に出来るか
+int solve(ll x) {
+    ll sum = 0;
+    ll cnt = 0;
+    ll i = 0;
+    for (; i < b.size(); i++) {
+        if (sum >= x) {
+            cnt++;
+            sum = 0;
+        }
+        if (cnt == k) break;
+        sum += b[i];
+    }
+    sum = 0;
+    for (; i < b.size(); i++) {
+        sum += b[i];
     }
 
-    sort(all(b), greater<pair<ll,ll>>());
+    if (sum < x) return false;
+    return true;
+}
 
-    for(auto x:b){
-        cout << "(" << x.first << "," << x.second << ") ";
-    }cout << endl;
+int megru_search() {
+    ll ok = 0;        //最小値求めるときは ok = INF
+    ll ng = 1e9 + 7;  //                   ng = -1
+    while (abs(ok - ng) > 1) {
+        ll mid = (ok + ng) / 2;
+        if (solve(mid)) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    return ok;
+}
+
+int main() {
+    cin >> n >> l;
+    cin >> k;
+    a = b = vector<ll>(n);
+    rep(i, 0, n) cin >> a[i];
+
+    b[0] = a[0];
+    rep(i, 0, n - 1) { b[i + 1] = a[i + 1] - a[i]; }
+    b.push_back(l - a[n - 1]);
+
+    ll ans = megru_search();
+    // putl(b);
+
+    // put(solve(14));
+
+    put(ans);
 }
