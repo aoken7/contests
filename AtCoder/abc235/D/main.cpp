f180f0f18cf9c4834bf92ea2b30d784bd9188383
aoken7 <cycloaoken@gmail.com>
2022-06-17 18:26:02 +0900

    modify template

---
+++ AtCoder/abc235/D/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define vec(T) vector<T>
+#define vec2(T) vector<vector<T>>
+#define all(a) a.begin(), a.end()
+#define put(i) cout << i << endl
+#define rep(i, s, n) for (long long i = s; i < (long long)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    
+}
